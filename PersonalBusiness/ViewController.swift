37ff9e85187069d88b78b0f5965a1d4275acfdfc
fsarror <fsarror@corp.vodafone.es>
2023-01-12 12:50:18 +0100

    Initial Commit

---
+++ PersonalBusiness/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PersonalBusiness
+//
+//  Created by Francisco Javier Sarró Redondo on 12/1/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
